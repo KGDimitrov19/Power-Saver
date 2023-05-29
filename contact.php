3b91b69ac61032b4f93e7f11f95774947f65bfa5
Кирил Георгиев Димитров <KGDimitrov19@codingburgas.bg>
2023-05-29 08:28:10 +0300

    Commit project

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<title></title>
+<body>
+
+<head>
+<meta charset="utf-8">
+	<meta name="author" content="anonymous">
+	<meta name="generator" content="Sublime text3">
+	<meta name="robot" content="index, follow">
+	<meta http-equiv="refresh" content="">
+	<meta name="description" content="This is navigation bar on html page">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keyowrds" content="html, navigation bar, navigation, bar, javascript, content">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="styleRegLog.css">
+
+</head>
+
+<header>
+    <div class="container">
+      <h1 class="logo"></h1>
+
+      <nav>
+        <ul>
+          <li><a href="index3.php">Home</a></li>
+          <li><a href="registration.php">Register</a></li>
+          <li><a href="login.php">Login</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <div class="google-map">
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5887.1297798257965!2d27.39964062110063!3d42.458273079814155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a6eb7e8a995853%3A0x9f3b508a4516b2ad!2z0J_QkyDQv9C-INC60L7QvNC_0Y7RgtGK0YDQvdC-INC_0YDQvtCz0YDQsNC80LjRgNCw0L3QtSDQuCDQuNC90L7QstCw0YbQuNC4!5e0!3m2!1sbg!2sbg!4v1683129350017!5m2!1sbg!2sbg" width="100" height="100" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> " width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+</body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+
+    <head>
+        <title>
+        </title>
+           
+        <title>Navigation bar</title>
+    <meta charset="utf-8">
+	<meta name="author" content="anonymous">
+	<meta name="generator" content="Sublime text3">
+	<meta name="robot" content="index, follow">
+	<meta http-equiv="refresh" content="">
+	<meta name="description" content="This is navigation bar on html page">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keyowrds" content="html, navigation bar, navigation, bar, javascript, content">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="styleIndex3.css">
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
+    </head>
+
+
+    <body>
+   
+    
+
+    <header class="header-area">
+  <!-- site-navbar start -->
+  <div class="navbar-area">
+    <div class="container">
+      <nav class="site-navbar">
+        <!-- site logo -->
+        <a href="#home" class="site-logo">PowerSaver</a>
+
+        <!-- site menu/nav -->
+        <ul>
+          <li><a href="index3.php">home</a></li>
+          <li><a href="registration.php">register</a></li>
+          <li><a href="login.php">login</a></li>
+          <li><a href="contact.php">contact</a></li>
+        </ul>
+
+        <!-- nav-toggler for mobile version only -->
+        <button class="nav-toggler">
+          <span></span>
+        </button>
+      </nav>
+    </div>
+  </div><!-- navbar-area end -->
+
+  <div class="intro-area">
+    <div class="container">
+      <h2>Let's make the Earth a better place</h2>
+      <p>Support us now</p>
+    </div>
+  </div>
+</header>
+
+
+  <h1 class="main-heading">Welcome to <span class="highlight">PowerSaver</span></h1>
+
+<section class="services">
+      <div class="card">
+          <img src="imgs/light.jpg" alt="Lights">
+          <h3 class="card-title">Lights</h3>
+          <p class="card-text">Regulating the power of your lights at home</p>
+       
+      </div>
+      <div class="card">
+          <img src="imgs/tmp.png" alt="Temperature">
+          <h3 class="card-title">Temperature</h3>
+          <p class="card-text">Regulating the temperature at home.</p>
+         
+      </div>
+
+
+
+
+      
+        </section>
+        <!-- Footer section -->
+<footer class="footer">
+   
+</section>
+
+
+
+
+</footer>
+    
+    </body>
+
+
+</html>
